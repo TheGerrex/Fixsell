fea27d963778241bfc5de2437bb1f21ae25d48d1
thegerrex <gerrex95@gmail.com>
2021-07-01 03:20:59 -0500

    agrege credenciales secretas para Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Heroku background image fix
+  config.serve_static_assets = true
+  config.assets.digest = true
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -26,7 +30,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
